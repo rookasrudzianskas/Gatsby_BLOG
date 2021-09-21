a87bde173e963fdfce4b725257c542a20a9bedd3
Rokas <rookas.rudzenskas@gmail.com>
2021-09-21 10:47:46 +0300

    Rokas

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://rookasdevblog.000webhostapp.comr/graphql`,
+          `https://wpgatsbydemo.wpengine.com/graphql`,
       },
     },
 
